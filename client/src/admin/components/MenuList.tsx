107a3315e5ffd1c14a15423e97bca2a9d64c97db
yuheesun <hesunlo@gmail.com>
2022-10-07 09:17:38 +0900

    Fix: 새로고침시 메뉴 리스트에서 현재 선택된 메뉴가 색 변화 안되는 에러 수정

--- client/src/admin/components/MenuList.tsx
+++ client/src/admin/components/MenuList.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { UseParams } from "../model/model";
 import { Menulist } from "../model/admin.model";
 
 type Props = {
@@ -9,6 +10,7 @@ type Props = {
 
 function MenuList({ menuList, logout }: Props) {
   let [clickedMenu, setClickedMenu] = useState<number>(1);
+  const path: string = useParams<UseParams>().id;
 
   return (
     <div className="adminMenuList">
@@ -18,7 +20,7 @@ function MenuList({ menuList, logout }: Props) {
           return (
             <Link to={pathStr}>
               <li
-                className={clickedMenu === key + 1 ? "clicked" : ""}
+                className={path === menu.path ? "clicked" : ""}
                 onClick={() => setClickedMenu(key + 1)}
               >
                 {menu.menu}
